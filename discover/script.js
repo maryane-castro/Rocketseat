ea5df5d932c716ad0b619b9e850bbb08e0bdfe0e
Maryane de Castro <122397850+maryane-castro@users.noreply.github.com>
2023-06-11 13:10:06 -0300

    .

--- discover/script.js
+++ discover/script.js
@@ -1,6 +1,6 @@
 function toggleMode(){
     const html = document.documentElement
-
+    
     // if(html.classList.contains('light')){
     //     html.classList.remove('light')
     // } else {
@@ -8,4 +8,14 @@ function toggleMode(){
     // }
 
     html.classList.toggle('light')
+
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains('light')){
+        img.setAttribute('src', './assets/avatar-light.png')
+    } else {
+        img.setAttribute('src', './assets/avatar-dark.png')
+    }
+
+
 }

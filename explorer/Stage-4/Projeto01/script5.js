36321e02be4b876bdc23e9d0ef1c4c7d1d573c61
Maryane de Castro <122397850+maryane-castro@users.noreply.github.com>
2023-07-03 18:20:58 -0300

    Update script5.js

--- explorer/Stage-4/Projeto01/script5.js
+++ explorer/Stage-4/Projeto01/script5.js
@@ -3,10 +3,18 @@ let weight;
 console.log(typeof weight);
 
 
-let nome = 'Nome'
+let nome = 'nome'
 let age = 15
 let stars = 123.32
-let isSubscribed = True
+let isSubscribed = true
 
-//object
-let student = {}
+// object
+let student = {
+    nome : 'NomeDaPessoa',
+    age : 15,
+    stars : 123.32,
+    isSubscribed : true
+}
+console.log(student)
+
+console.log(`Nome: ${student.nome}, Idade: ${student.idade}, Inscrito: ${student.isSubscribed}`)

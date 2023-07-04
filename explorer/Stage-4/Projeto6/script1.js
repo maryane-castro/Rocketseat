333c29d4e5142d5c827475f2fc9e5d4a33ad9630
Maryane de Castro <122397850+maryane-castro@users.noreply.github.com>
2023-07-03 21:18:11 -0300

    Update script1.js

--- explorer/Stage-4/Projeto6/script1.js
+++ explorer/Stage-4/Projeto6/script1.js
@@ -1,3 +1,31 @@
-for (let i = 5; i < 10; i++){
-    console.log(i)
+// for (let i = 1; i < 10; i++){
+//     if (i === 5){
+//         break;
+//     }
+//     console.log(i)
+// }
+
+// let a = 0
+// while (a < 10){
+//     console.log(a)
+//     a++;
+// }
+
+
+// let name = 'maik'
+// let names = ['joao', 'paulo', 'pedro']
+// for(let char of names[1]){
+//     console.log(char)
+// }
+
+
+let person = {
+    name: 'josh',
+    age: 30,
+    peso: 6.5
+}
+
+for (let propety in person){
+    console.log(propety)
+    console.log(person[propety])
 }

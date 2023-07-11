5b9105fdcdf0eb1653dbd6ddbaed1b1a6225bb2d
maryane-castro <maryane.castro993@gmail.com>
2023-07-11 14:50:47 -0300

    .

--- explorer/Stage-5/Projeto02/index.js
+++ explorer/Stage-5/Projeto02/index.js
@@ -1,17 +1,15 @@
 const img = document.querySelector("#img")
 const button = document.querySelector('#button')
+const lista = ['Linux', 'macOS', 'Windows']
+
 img.addEventListener('click', trocar)
 button.addEventListener('click', voltar)
-
-
-const lista = ['Linux', 'macOS', 'Windows']
 
 function trocar(event){
     event.preventDefault()
     document.querySelector('.screen1').classList.toggle("hide")
     document.querySelector('.screen2').classList.toggle("hide")
     elemento = lista[Math.floor(Math.random() * lista.length)]
-
     document.querySelector(".screen2 p").innerText = `${elemento}`
 }
 

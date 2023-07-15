6bf1408d118ce7177fe982f9fd88f731441fdadd
maryane-castro <maryane.castro993@gmail.com>
2023-07-15 09:45:46 -0300

    .

--- explorer/Stage-6/Projeto02/js/Favorites.js
+++ explorer/Stage-6/Projeto02/js/Favorites.js
@@ -1,18 +1,4 @@
-export class GithubUser {
-    static search(username) {
-        const endpoint = `https://api.github.com/users/${username}`
-
-        return fetch(endpoint)
-        .then(data => data.json())
-        .then(({login, name, public_repos, followers}) => ({
-            login,
-            name,
-            public_repos,
-            followers            
-        }))
-        //.catch(e => alert('Encontrei um erro', e))
-    }
-}
+import { GithubUser } from "./GithubSearch.js"
 
 
 export class Favorites{
@@ -39,6 +25,14 @@ export class Favorites{
     //async : codigo assíncrono
     async add(username){
         try{
+
+            const userExists = this.entries.find(entry => entry.login === username)
+            
+
+            if (userExists){
+                throw new Error('Usuário já cadastrado')
+            }
+
             const user = await GithubUser.search(username)
             if (user.login === undefined){
                 throw new Error('Usuário não encontrado')
@@ -91,6 +85,8 @@ export class FavoritesView extends Favorites{
             const row = this.createRow()
             row.querySelector('.user img').src = `https://github.com/${user.login}.png`
             row.querySelector('.user img').alt = `Imagem de ${user.login}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
+
             row.querySelector('.user p').textContent = user.name
             row.querySelector('.user span').textContent = user.login
             row.querySelector('.repositories').textContent = user.public

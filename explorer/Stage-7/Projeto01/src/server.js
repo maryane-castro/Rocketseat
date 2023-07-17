7a6756533bf3d3a0195abd45efdb052a42765f27
maryane-castro <maryane.castro993@gmail.com>
2023-07-17 08:08:51 -0300

    sql

--- explorer/Stage-7/Projeto01/src/server.js
+++ explorer/Stage-7/Projeto01/src/server.js
@@ -5,10 +5,11 @@ const AppError = require('./utils/AppError')
 
 const express = require("express");
 const routes = require("./routes")
-
+const database = require("./database/sqlite")
 const app = express();
 app.use(express.json())
 app.use(routes)
+database();
 
 
 app.use((error, request, response, next) => {

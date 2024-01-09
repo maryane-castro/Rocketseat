1fe6681f9054a213317b8f36f1c5b99fbb924f56
maryane-castro <maryane.castro993@gmail.com>
2024-01-09 17:11:37 -0300

    ;

--- react-native/Stage-2/my-app/src/components/GrupCard/index.tsx
+++ react-native/Stage-2/my-app/src/components/GrupCard/index.tsx
@@ -5,9 +5,9 @@ type Props = TouchableOpacityProps & {
     title: string;
 }
 
-export function GrupCard({ title }: Props){
+export function GrupCard({ title, ...rest }: Props){
     return(
-        <Container>
+        <Container {...rest}>
             <Icon/>
             <Title>
                 {title}
--- react-native/Stage-2/my-app/src/screens/Groups/index.tsx
+++ react-native/Stage-2/my-app/src/screens/Groups/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GrupCard } from '@components/GrupCard';
 import { Container } from './styles';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>(['rock'])
+
+
   return (
     <Container>
       <Header/>
